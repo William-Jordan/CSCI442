28953c4dae3185b9f5a5104cd9cbd5695fb78ea0
ABat16 <66046058+ABat16@users.noreply.github.com>
2023-04-28 12:40:04 -0600

    Add files via upload

--- Testing.py
+++ Testing.py
@@ -18,21 +18,29 @@ turn = 6000
 
 forward = 5300
 backward = 6800
-left = 6900
-right = 5100
+left = 7000
+right = 5000
 stop = 6000
 
+#Small rotate t = .5
+#90 degree rotate t = 1.5
+90t = 1.5
+tick = .3
 rotate = True
+#Rotates little
 if rotate:
     tango.setTarget(MOTORS, stop)
     tango.setTarget(TURN, left)
-    time.sleep(.1)
+    time.sleep(90t)
     tango.setTarget(TURN, stop)
+    for i in range(10):
+        tango.setTarget(TURN, right)
+        tango.sleep(tick)
+        tango.setTarget(TURN, stop)
 
 tango.setTarget(TURN, stop)
 tango.setTarget(MOTORS, stop)
 tango.setTarget(HEADTURN, stop)
 tango.setTarget(HEADTILT, stop)
 tango.setTarget(BODY, stop)
-video.release()
-cv.destroyAllWindows()
+
