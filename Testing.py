e58359eba35205e9f855c923903b523434af2c9f
ABat16 <66046058+ABat16@users.noreply.github.com>
2023-04-28 12:46:27 -0600

    Add files via upload

--- Testing.py
+++ Testing.py
@@ -24,7 +24,7 @@ stop = 6000
 
 #Small rotate t = .5
 #90 degree rotate t = 1.5
-90t = 1.5
+t90 = 1.5
 tick = .3
 rotate = True
 #Rotates little
@@ -33,10 +33,9 @@ if rotate:
     tango.setTarget(TURN, left)
     time.sleep(90t)
     tango.setTarget(TURN, stop)
-    for i in range(10):
-        tango.setTarget(TURN, right)
-        tango.sleep(tick)
-        tango.setTarget(TURN, stop)
+    tango.setTarget(TURN, right)
+    time.sleep(tick)
+    tango.setTarget(TURN, stop)
 
 tango.setTarget(TURN, stop)
 tango.setTarget(MOTORS, stop)
